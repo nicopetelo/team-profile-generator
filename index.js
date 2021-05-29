b3b317637027eb225fee278a7df9fcbcc690d3bf
nicopetelo <nicopetelo@gmail.com>
2021-05-29 10:22:52 -0700

    set up paths and fs and started classes

---
+++ index.js
@@ -0,0 +1,12 @@
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+
+const outputPath = path.resolved(__dirname, "output", 'team.html');
+// require intern
+//require path
+//require fs
+//require inquirer
+
+//require html.js
+
+const teamMembers = [];
